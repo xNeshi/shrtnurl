6b0eda0d876247c6a9ddce33ab86851e70314524
Liam Viray <neshiviray@gmail.com>
2025-06-02 02:50:05 +0800

    Added methods to UrlService

--- src/main/java/com/xneshi/shrtnurl/service/UrlService.java
+++ src/main/java/com/xneshi/shrtnurl/service/UrlService.java
@@ -1,12 +1,32 @@
 package com.xneshi.shrtnurl.service;
 
+import com.xneshi.shrtnurl.dto.UrlRequestDTO;
+import com.xneshi.shrtnurl.dto.UrlResponseDTO;
+import com.xneshi.shrtnurl.mapper.UrlMapper;
+import com.xneshi.shrtnurl.model.Url;
 import com.xneshi.shrtnurl.respository.UrlRepository;
+import com.xneshi.shrtnurl.util.ShortenerUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class UrlService {
   private final UrlRepository urlRepository;
 
+  public UrlResponseDTO shortenUrl(UrlRequestDTO url) {
+    var shortenedUrl = UrlMapper.toUrl(url);
+    shortenedUrl.setCreatedAt(LocalDateTime.now());
+    shortenedUrl.setShortCode(ShortenerUtil.hashUrl(url.originalUrl()));
+    urlRepository.save(shortenedUrl);
+
+    return UrlMapper.toUrlResponseDTO(shortenedUrl);
+  }
+
+  public String findOriginalUrl(String shortCode) {
+    Url url = urlRepository.findByShortCode(shortCode);
+    return url.getOriginalUrl();
+  }
 }
