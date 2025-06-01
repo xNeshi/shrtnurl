bc0b94804ec711f328ef6b30ff2ec08b659222c7
Liam Viray <neshiviray@gmail.com>
2025-06-02 02:48:27 +0800

    Updated UrlRequestDTO

--- src/main/java/com/xneshi/shrtnurl/dto/UrlRequestDTO.java
+++ src/main/java/com/xneshi/shrtnurl/dto/UrlRequestDTO.java
@@ -1,11 +1,8 @@
 package com.xneshi.shrtnurl.dto;
 
-import com.xneshi.shrtnurl.dto.validators.ShortenerUrlValidationGroup;
 import jakarta.validation.constraints.NotBlank;
 
 public record UrlRequestDTO(
-    @NotBlank
-    String shortCode,
     @NotBlank
     String originalUrl
 ) {
