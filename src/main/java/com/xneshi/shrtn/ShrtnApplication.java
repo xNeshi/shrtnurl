573f4b7cfb2989becaeb7ef8b2691fad959d9224
Liam Viray <neshiviray@gmail.com>
2025-06-01 21:59:48 +0800

    init commit

---
+++ src/main/java/com/xneshi/shrtn/ShrtnApplication.java
@@ -0,0 +1,13 @@
+package com.xneshi.shrtn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrtnApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShrtnApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/xneshi/shrtn/ShrtnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xneshi.shrtn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrtnApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
