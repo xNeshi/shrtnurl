0f305affc341ffdebf75335aade7adb754c0c09a
Liam Viray <neshiviray@gmail.com>
2025-06-02 02:51:14 +0800

    Updated UrlController

--- src/main/java/com/xneshi/shrtnurl/controller/UrlController.java
+++ src/main/java/com/xneshi/shrtnurl/controller/UrlController.java
@@ -1,9 +1,15 @@
 package com.xneshi.shrtnurl.controller;
 
+import com.xneshi.shrtnurl.dto.UrlRequestDTO;
+import com.xneshi.shrtnurl.dto.UrlResponseDTO;
 import com.xneshi.shrtnurl.service.UrlService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,16 +17,18 @@ public class UrlController {
   private final UrlService urlService;
 
   @PostMapping("/shorten")
-  public ResponseEntity<String> shortenUrl(
-      @RequestBody String url
+  public ResponseEntity<UrlResponseDTO> shortenUrl(
+      @Valid @RequestBody UrlRequestDTO url
   ) {
-    return ResponseEntity.ok("");
+    return ResponseEntity.ok(urlService.shortenUrl(url));
   }
 
   @GetMapping("/{shortCode}")
-  public ResponseEntity<String> fetchOriginalUrl(
+  public ResponseEntity<Void> redirectToOriginalUrl(
       @PathVariable String shortCode
   ) {
-    return ResponseEntity.ok("");
+      return ResponseEntity.status(HttpStatus.FOUND)
+          .location(URI.create(urlService.findOriginalUrl(shortCode)))
+          .build();
   }
 }
