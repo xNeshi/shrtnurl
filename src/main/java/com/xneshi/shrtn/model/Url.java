9eaec844d0599313d94f920436650c64a8605692
Liam Viray <neshiviray@gmail.com>
2025-06-01 22:23:39 +0800

    Update Config, Added Url Entity

---
+++ src/main/java/com/xneshi/shrtn/model/Url.java
@@ -0,0 +1,23 @@
+package com.xneshi.shrtn.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Url {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+  private String originalUrl;
+  private String shortCode;
+}
